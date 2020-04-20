a28db080f36ae8e72d362d5b794967aeec7f8cb8
Charles Pelton <charliejp0311@gmail.com>
2020-04-20 20:57:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,12 +7,14 @@ class App < Sinatra::Base
   end
 
   get '/' do
-    @session = session
+
     erb :index
   end
 
   post '/checkout' do
+    @session = session
     @session[:item] = params[:item]
+    # binding.pry
     erb :checkout
   end
 end
