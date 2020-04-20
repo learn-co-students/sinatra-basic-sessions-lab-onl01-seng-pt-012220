385c1a4411d02fd01a118283a13b116031ff72e4
Charles Pelton <charliejp0311@gmail.com>
2020-04-20 17:36:13 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  configure do
+  enable :sessions
+
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/checkout' do
+    @items = params
+    binding.pry
+    erb :checkout
+  end
 end
