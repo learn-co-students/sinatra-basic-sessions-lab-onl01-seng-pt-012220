0c5836897a4170eb8a33389a560b251841cfcc13
Hailey Bongo <haileyebongo@gmail.com>
2020-04-23 17:45:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
+
 class App < Sinatra::Base
+  
+  #attr_accessor :session 
+  
+  configure do
+  enable :sessions 
+  set :session_secret, "secret"
+  end
+
+
+  
+  get '/' do 
+    erb :index
+  end
+  
+  post'/checkout' do
+    @session = session
+    @session["item"] = params["item"]
+    erb :display_item
+  end
 end
